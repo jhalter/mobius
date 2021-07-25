8b53c840617ef7c2b14b197759d3c1722fe478a1
Jeff Halter <868228+jhalter@users.noreply.github.com>
2021-07-24 18:32:10 -0700

    Fix banner embed

--- client.go
+++ client.go
@@ -344,7 +344,7 @@ func randomBanner() string {
 	rand.Seed(time.Now().UnixNano())
 
 	bannerFiles, _ := bannerDir.ReadDir("client/banners")
-	file, _ := os.ReadFile("banners/" + bannerFiles[rand.Intn(len(bannerFiles))].Name())
+	file, _ := bannerDir.ReadFile("client/banners/" + bannerFiles[rand.Intn(len(bannerFiles))].Name())
 
 	return fmt.Sprintf("\n\n\nWelcome to...\n\n[red::b]%s[-:-:-]\n\n", file)
 }
